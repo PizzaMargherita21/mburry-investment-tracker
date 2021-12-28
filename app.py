29387cf6370d06e04e4e3fc1d25001b20e132c21
PizzaMargherita21 <56775464+PizzaMargherita21@users.noreply.github.com>
2021-12-28 16:44:19 +0100

    bug fix & added new feature
    added new feature to check Korean stock tickers for Burry's international investments

--- app.py
+++ app.py
@@ -21,36 +21,41 @@ st.set_page_config(
 
 st.title("M.Burry Investment Tracker")
 
-
 #Option single ticker or ticker list loaded via CSV
-options = ['Single Ticker Check', 'Multi Ticker Check via CSV upload']
-getTickerOptions = st.radio('Select Ticker Check option', options)
-if getTickerOptions == 'Single Ticker Check':
-    tickerList = []
-    #Check via single ticker
-    x = st.text_input('Enter Ticker')
-    if x:
-        tickerList.append(x)
-    else:
-        st.info("Please anter a ticker")
+def getTicker():
+    options = ['Single Ticker Check', 'Multi Ticker Check via CSV upload']
+    getTickerOptions = st.radio('Select Ticker Check option', options, key="0")
+    if getTickerOptions == 'Single Ticker Check':
+        tickerList = []
+        #Check via single ticker
+        x = st.text_input('Enter Ticker')
+        if x:
+            tickerList.append(x)
+        else:
+            st.info("Please enter a ticker")
 
-elif getTickerOptions == 'Multi Ticker Check via CSV upload':
-    tickerList = []
-    try:
-        #read csv
-        uploaded_file = st.file_uploader('Choose a file')
-        colnames=['Ticker','Name','Asset Class','Weight (%)','Price','Shares','Market Value','Notional Value','Sector','SEDOL','ISIN','Location','Currency','Market Currency','FX Rate'] 
-        tickerlist=pd.read_csv(uploaded_file, sep=',',skiprows=10,skipfooter=2, names=colnames, header=None)
-        tickerList = tickerlist[['Ticker','Name']]
-        tickerList = tickerList['Ticker']
-    
-    except:
-        st.warning('Please upload a csv file')
+    elif getTickerOptions == 'Multi Ticker Check via CSV upload':
+        tickerList = []
+        try:
+            #read csv
+            uploaded_file = st.file_uploader('Choose a file')
+            colnames=['Ticker','Name','Asset Class','Weight (%)','Price','Shares','Market Value','Notional Value','Sector','SEDOL','ISIN','Location','Currency','Market Currency','FX Rate'] 
+            tickerlist=pd.read_csv(uploaded_file, sep=',',skiprows=10,skipfooter=2, names=colnames, header=None)
+            tickerList = tickerlist[['Ticker','Name']]
+            tickerList = tickerList['Ticker']
+
+        except:
+            st.warning('Please upload a csv file')
+
+    return tickerList
 
 #Menu
 st.sidebar.header("Menu")
 menu = ["Burry Investments", "Burry Style Analysis","About"]
 choice = st.sidebar.selectbox("Select Page",menu)
+
+countryList = ['Japan', 'Korea']
+selectCountry = st.sidebar.radio('Select Country', countryList, key="1")
 
 #External links
 st.sidebar.header('External Links')
@@ -63,22 +68,30 @@ st.sidebar.write(msciJapanValueETF)
 if choice == "Burry Investments":
     
     st.header("Burry Investments :eyes:")
+    
+    #Get Ticker
+    tickerList = getTicker()
 
     def main(ticker):
         listOwnership = pd.DataFrame(columns=['Company Ticker', 'Link'])
+        st.text('\n')
         latest_iteration = st.empty()
         bar = st.progress(0)
         for idx,ticker in enumerate(ticker):
             
-            latest_iteration.text('Status Ticker Check: {} Ticker left from {}'.format((len(tickerList) - (idx+1)), (len(tickerList))))
+            latest_iteration.write('Progress Bar: {} Ticker left from {}'.format((len(tickerList) - (idx+1)), (len(tickerList))))
             bar.progress((100//len(tickerList))*(idx+1))
-            time.sleep(0.1)
+            time.sleep(1)
             
             try:
                 #st.write(idx, ticker)
 
                 # Get url:
-                urlOwnership = 'https://www.wsj.com/market-data/quotes/JP/'+ticker+'/company-people'
+                if selectCountry == 'Japan':
+                    urlOwnership = 'https://www.wsj.com/market-data/quotes/JP/'+ticker+'/company-people'
+                    
+                elif selectCountry == 'Korea':
+                    urlOwnership = 'https://www.wsj.com/market-data/quotes/KR/XKRX/'+ticker+'/company-people'
 
                 # Get text-based page content of web-files 
                 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} 
@@ -97,13 +110,15 @@ if choice == "Burry Investments":
                         #listOwnership[ticker] = name
                         listOwnership  = listOwnership.append({'Company Ticker': ticker, 'Link': urlOwnership}, ignore_index=True)
                         st.text('\n')
-                        st.write('Yes, it seems that M. Burry is invested in **{}**'.format(ticker))
+                        st.write('## :fire: It seems that Burry is betting on these stock(s) :fire:')
               
 
             except:
                 pass
-            
-        st.write('# List of Burry Ownership', listOwnership)
+        
+        st.text('\n')
+        st.write('### List of Burry Ownership')
+        st.write(listOwnership)
 
     if len(tickerList)>=1:
         main(tickerList)
@@ -114,20 +129,46 @@ if choice == "Burry Investments":
 if choice == "Burry Style Analysis":  
     
     st.header("Burry Style Analysis :mag:")
-    st.subheader("Collecing Ideas, More Soooooon")
     
-    #Select date
-    st.subheader("Date")
-    startDate = st.date_input('Start Date', datetime(2019, 1, 1))
-    endDate = st.date_input('End Date')
+    #Get Ticker
+    x = st.text_input('Enter Ticker')
+    if x:
+        ticker = x
+        
+        #Select date
+        st.subheader("Time Period")
+
+        col1, col2 = st.beta_columns(2) 
+        with col1:
+            startDate = st.date_input('Start Date', datetime(2019, 1, 1))
+        with col2:
+            endDate = st.date_input('End Date')
+
+        if selectCountry == 'Japan':
+            data = pdr.get_data_yahoo(ticker+'.T', startDate,endDate)['Adj Close']
+            currency = 'JPY'
+                    
+        elif selectCountry == 'Korea':
+            data = pdr.get_data_yahoo(ticker+'.KQ', startDate,endDate)['Adj Close']
+            currency = 'KRW'
+            
+        if not data.empty:
+            
+            priceChart = px.line(data, x=data.index, y='Adj Close')
+            priceChart.update_layout(
+                title="Daily (Closing) Price Chart",
+                yaxis_title= "Price {}".format(currency),
+                legend_title="Ticker")
+
+            st.write(priceChart)
+            
+        else:
+            st.write('## No price data available yet!')
     
-    data = pdr.get_data_yahoo(tickerList[0]+'.T', startDate,endDate)['Adj Close']
-    priceChart = px.line(data, x=data.index, y='Adj Close')
-    priceChart.update_layout(
-        title="Daily (Closing) Price Chart",
-        yaxis_title="Price (JPY)",
-        legend_title="Ticker")
-    st.write(priceChart)
+    else:
+        st.info("Please enter a ticker")
+    
+    st.write("# :bulb: Collecing Ideas, More Soooooon :bulb:")
     
     
 elif choice == "About":
